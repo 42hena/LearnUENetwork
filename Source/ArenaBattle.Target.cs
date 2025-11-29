f59b22b13719afea389c773cd36c3658414ffd02
soloplay <somnium1108@gmail.com>
2025-11-29 18:22:59 +0900

    Feat: 네트워크 멀티 플레이 프레임워크의 이해
    - UE Version 변경 (5.1 -> 5.4)
    - Editor 및 Target 파일 변경 (5.1로 되어있음)
    - 헤더 문제 해결 -> OverlapResult 헤더 추가하여 빌드 성공
    - 네트워크 세팅 변경
     - Netmode 리슨 서버로 변경
     - Allow late joining 설정하여 실행 중 클라이언트 가능하도록 하기
     - Always On Top 설정하여 최상단에 올리기

---
+++ Source/ArenaBattle.Target.cs
@@ -0,0 +1,15 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ArenaBattleTarget : TargetRules
+{
+	public ArenaBattleTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		ExtraModuleNames.Add("ArenaBattle");
+	}
+}
---
+++ Source/ArenaBattleEditor.Target.cs
@@ -0,0 +1,15 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ArenaBattleEditorTarget : TargetRules
+{
+	public ArenaBattleEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		ExtraModuleNames.Add("ArenaBattle");
+	}
+}
